65980aec9159c5f6f9ba1b058b19c15db99e64f2
Stephlzn <stephanylezamaninatanta@gmail.com>
2025-06-17 23:29:05 -0500

    Cambios actualizar/inventario

--- OneDrive/Documentos/NetBeansProjects/AnaceOriginal-master/AnaceOriginal-master/src/main/java/libreriaanace/GUIInventario.java
+++ OneDrive/Documentos/NetBeansProjects/AnaceOriginal-master/AnaceOriginal-master/src/main/java/libreriaanace/GUIInventario.java
@@ -159,7 +159,7 @@ public class GUIInventario extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
-        
+        new GUIActualizarProduct().setVisible(true);
     }//GEN-LAST:event_jButton8ActionPerformed
 
     /**
